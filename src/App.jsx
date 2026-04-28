d54082079078181862fa7cb6794410f7d4be27fe
fiqhipondaa9 <fiqhipondaag@gmail.com>
2026-04-29 01:36:49 +0800

    OKOK UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -213,7 +213,7 @@ export default function App() {
               PERMENPORA 15 / 2024
             </div>
             <h1 className="text-4xl md:text-6xl font-black tracking-tighter leading-tight italic">
-              VELOCITY <span className="text-rose-500">ATHLETICS</span>
+              KALKULATOR FISIK <span className="text-rose-500"> ATLETIK</span>
             </h1>
           </div>
           <div className="text-left md:text-right w-full md:w-auto">
